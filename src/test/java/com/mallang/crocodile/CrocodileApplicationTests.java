58cc1bf0325e701a7076788dd60f809c4f95f25d
김동주 <dongju.k@navercorp.com>
2023-07-23 01:58:54 +0900

    프로젝트 생성

---
+++ src/test/java/com/mallang/crocodile/CrocodileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mallang.crocodile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrocodileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
