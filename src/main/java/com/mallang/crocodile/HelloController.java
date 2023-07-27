4f9db4f3ef45443869efd46e1d4351e6eec8544b
김동주 <dongju.k@navercorp.com>
2023-07-28 01:24:29 +0900

    Lombok 적용

--- src/main/java/com/mallang/crocodile/HelloController.java
+++ src/main/java/com/mallang/crocodile/HelloController.java
@@ -3,10 +3,14 @@ package com.mallang.crocodile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HelloController {
 	@GetMapping("/hello")
 	public String hello() {
+		log.info("hello~");
 		return "hello";
 	}
 }
