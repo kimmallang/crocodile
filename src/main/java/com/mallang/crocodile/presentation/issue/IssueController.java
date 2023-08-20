b8d4cfe9f2f5da838795cfd06392e41556332a43
김동주 <dongju.k@navercorp.com>
2023-08-20 19:53:19 +0900

    이슈 API - 목록 조회

---
+++ src/main/java/com/mallang/crocodile/presentation/issue/IssueController.java
@@ -0,0 +1,64 @@
+package com.mallang.crocodile.presentation.issue;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mallang.crocodile.exception.BadRequestException;
+import com.mallang.crocodile.exception.NotFoundException;
+import com.mallang.crocodile.presentation.ApiResponse;
+import com.mallang.crocodile.presentation.category.CategoryListView;
+import com.mallang.crocodile.presentation.category.CategoryRequest;
+import com.mallang.crocodile.presentation.category.CategoryView;
+import com.mallang.crocodile.presentation.document.ErrorResponse400_403_404_500;
+import com.mallang.crocodile.presentation.document.ErrorResponse400_404_500;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "이슈")
+@RequestMapping("/api")
+@RestController
+public class IssueController {
+	@GetMapping("/issues")
+	@ErrorResponse400_404_500
+	@ApiOperation(value = "이슈 목록 조회")
+	public ApiResponse<IssueListView> getCategories(
+		@RequestParam(defaultValue = "0") Long cursor,
+		@RequestParam(defaultValue = "20") Integer pageSize) {
+		List<IssueView> issueViews = new ArrayList<>();
+		for (int i = 0; i < pageSize; i++) {
+			issueViews.add(IssueView.builder()
+				.id((long)i)
+				.categoryId(1L)
+				.name("이슈 이름")
+				.description("이슈 설명")
+				.assigneeUserId("red")
+				.ownerUserId("blue")
+				.hashTags(Arrays.asList("해시태그1", "해시태그2", "해시태그3"))
+				.createdAt(LocalDateTime.of(2021, 9, 14, 15, 30))
+				.modifiedAt(LocalDateTime.of(2021, 9, 14, 15, 30))
+				.build());
+		}
+
+		return ApiResponse.<IssueListView>builder()
+			.data(IssueListView.builder()
+				.isLast(true)
+				.cursor(cursor)
+				.nextCursor(0L)
+				.pageSize(pageSize)
+				.totalPage(10)
+				.totalCount(200)
+				.issues(issueViews)
+				.build())
+			.build();
+	}
+}
